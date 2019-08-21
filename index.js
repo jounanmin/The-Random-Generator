faa26f716436e61d911f3e903472b2661f50f6cd
jonamin <jounanmin@hotmail.com>
2019-08-21 23:31:40 +0000

    Linked JS w HTML

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 console.log("Hello World");
 
 let who = ['the dog','my granma','his turtle','my bird'];
-let what = ['eat','pissed','crushed','broked'];
+let action = ['ate','pissed','crushed','broked'];
+let what = ['my car','my keys','my homework','the poem'];
 let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
 
-let excuse1 = who[Math.floor(Math.random() * who.length)] + " " + what[Math.floor(Math.random() * what.length)] + "  " + when[Math.floor(Math.random() * when.length)];
-let excuse2 = who[Math.floor(Math.random() * who.length)] + " " + what[Math.floor(Math.random() * what.length)] + "  " + when[Math.floor(Math.random() * when.length)];
+let excuse = who[Math.floor(Math.random() * who.length)] + " "
+            + action[Math.floor(Math.random() * action.length)] + "  "
+            + what[Math.floor(Math.random() * what.length)] + "  "
+            + when[Math.floor(Math.random() * when.length)];
 
-console.log(excuse1);
-console.log(excuse2);
+console.log(excuse);
+
+let message = document.querySelector("#excuse");
+message.innerHTML = excuse;
